38c830d4eebaee99cc676d895c457ad32fbad644
unknown <julianjuarez98.jj@gmail.com>
2021-05-26 22:36:50 -0300

    se completo CRUD de clientes y se añadio template Plan

--- membresia/models.py
+++ membresia/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import  date
 
 # Create your models here.
 class Planes(models.Model):
@@ -19,14 +20,14 @@ class Cliente(models.Model):
     last_name= models.CharField(max_length=60,verbose_name="Apellido")
     Tipo_plan = models.ForeignKey(Planes, on_delete=models.CASCADE)
     #modelChoicefield
-    fecha_inicio=models.DateField(auto_now=False, verbose_name="Fecha de inicio")
+    fecha_inicio=models.DateField(auto_now=False, verbose_name="Fecha de inicio",default=date.today)
     fecha_fin=models.DateField(auto_now=False, verbose_name="Fecha de finalizacion")
     state=models.BooleanField('Activo',default=True) 
     
     class Meta:
         verbose_name= "Cliente"
-    verbose_name_plural = "Clientes"
-    ordering = ["-created"]
+        verbose_name_plural = "Clientes"
+        ordering = ["name"]
         
     def __str__(self):
         return self.name
--- membresia/views.py
+++ membresia/views.py
@@ -1,37 +1,110 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, DetailView, CreateView
-from .models import Cliente
+from django.views.generic import (TemplateView,
+                                ListView,
+                                DetailView,
+                                CreateView,
+                                DeleteView,
+                                UpdateView)
+from .models import Cliente, Planes
 
 # Create your views here.
+
 
 class inicioView(TemplateView):
     template_name = "inicio.html"
 
 
 class pruebaView(ListView):
-    template_name='clientes_all.html'
-    model= Cliente
+    template_name = 'clientes_all.html'
+    model = Cliente
     paginate_by = 6
-    context_object_name="Lista_clientes"
-    
+    context_object_name = "Lista_clientes"
+    ordering = '-last_name'
+
     def get_queryset(self):
         palabra_clave = self.request.GET.get("kword", '')
         lista = Cliente.objects.filter(
-        name__icontains=palabra_clave
-    )
+            name__icontains=palabra_clave
+        )
         return lista
 
+
+class adminClienteView(ListView):
+    template_name = 'clientes_admin.html'
+    model = Cliente
+    paginate_by = 6
+    context_object_name = "Lista_clientes"
+
+    def get_queryset(self):
+        palabra_clave = self.request.GET.get("kword", '')
+        lista = Cliente.objects.filter(
+            name__icontains=palabra_clave
+        )
+        return lista
 
 class ClienteDetailView(DetailView):
     model = Cliente
     template_name = "detail_cliente.html"
 
+
 class successView(TemplateView):
     template_name = "success.html"
+
 
 class ClienteCreateView(CreateView):
     model = Cliente
     template_name = "add_cliente.html"
-    fields=("__all__")
-    success_url = reverse_lazy('clientes_app:correcto')
+    fields = ("__all__")
+    success_url = reverse_lazy('clientes_app:inicio')
+
+
+class PlanesListView(ListView):
+    model = Planes
+    template_name = "planes_all.html"
+
+
+class ClienteUpdateView(UpdateView):
+    model = Cliente
+    template_name = "update_cliente.html"
+    fields = [
+        'name',
+        'last_name',
+        'Tipo_plan',
+        'fecha_inicio',
+        'fecha_fin',
+        "state",
+    ]
+    success_url = reverse_lazy('clientes_app:clientes_admin')
+
+    def post(self, request, *args, **kwargs):
+        print('*************************')
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        # logica del proceso
+        return super(ClienteUpdateView, self).form_valid(form)
+
+
+class ClienteDeleteView(DeleteView):
+    model = Cliente
+    template_name = "cliente_delete.html"
+    success_url = reverse_lazy('clientes_app:clientes_admin')
+
+
+class PlanesListView(ListView):
+    model = Planes
+    template_name = "list_planes.html"
+    context_object_name= "planes"
+    
+
+class ClientesPorPlanListView(ListView):
+    template_name = "list_by_plan.html"
+    context_object_name="clientes"
+    def get_queryset(self):
+        # el codigo que yo quiera
+        plan = self.kwargs['id']
+        lista = Cliente.objects.filter(
+            Tipo_plan_id=plan
+        )
+        return lista
