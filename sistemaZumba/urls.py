0c3fc005389a3538c0d1e5dc9a1ef723b9defda5
unknown <julianjuarez98.jj@gmail.com>
2021-08-09 00:28:29 -0300

    conf para static 14

--- sistemaZumba/urls.py
+++ sistemaZumba/urls.py
@@ -18,12 +18,12 @@ from django.urls import path
 from django.urls import path, re_path,include
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('',include('membresia.urls')),
     path('', LoginView.as_view(template_name='form-login.html'), name="login"),
     path('logout/', LogoutView.as_view(), name="logout")
-] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
