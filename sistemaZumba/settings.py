861bbf1244936ebdd597e64ce7788a49d672c291
unknown <julianjuarez98.jj@gmail.com>
2021-08-08 22:39:55 -0300

    conf para static 6

--- sistemaZumba/settings.py
+++ sistemaZumba/settings.py
@@ -133,11 +133,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 #STATICFILES_DIRS=['static']
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+#STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+#STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_URL = reverse_lazy('login')
 LOGIN_REDIRECT_URL =reverse_lazy('clientes_app:inicio')
