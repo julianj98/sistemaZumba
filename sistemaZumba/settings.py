ba412d677b9a5280338ac43cd8fcce2adc8d8db2
unknown <julianjuarez98.jj@gmail.com>
2021-08-15 18:41:39 -0300

    probando 15/8

--- sistemaZumba/settings.py
+++ sistemaZumba/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY=config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['zumbaconnancy.herokuapp.com']
+ALLOWED_HOSTS = ['www.zumbaconnancy.herokuapp.com']
 
 
 # Application definition
